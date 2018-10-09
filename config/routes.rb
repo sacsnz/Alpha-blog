f50012e1c255729c74edb34fc6e17ed9922e8896
sacsnz <sales@worksites.co.nz>
2018-10-09 10:18:36 +0000

    Adding article new page

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   
-  get 'home', to: 'pages#home'
+  root 'pages#home'
   get 'about', to: 'pages#about'
+  
+  resources :articles
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
