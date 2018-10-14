036d41d728e638c2027e2cfb301dc19789bc656d
sacsnz <sales@worksites.co.nz>
2018-10-14 09:14:14 +0000

    Added edit and articles pages

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   validates :title, presence:true, length:{minimum: 3, maximum: 50}
-  validates :title, presence:true, length:{minimum: 3, maximum: 200}
+  validates :description, presence:true, length:{minimum: 3, maximum: 200}
   
   
 end
